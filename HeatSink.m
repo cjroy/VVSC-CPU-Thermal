6dbaf38742870dc5d8dfd0dd183fc4088c7d6e09
Chris Roy <cjroy@vt.edu>
2014-02-07 08:13:12 -0500

    to increase TDP and # fins (X2) and change thermal conductivity of CPU

--- HeatSink.m
+++ HeatSink.m
@@ -4,8 +4,8 @@ clc
 %============Inputs============
 
 %**System Paremeters**
-TDP = 125.0;           % Thermal Design Power, W
-NumFins = 5;           % Number of heat sink fins
+TDP = 250.0;           % Thermal Design Power, W
+NumFins = 10;           % Number of heat sink fins
 L = 8/100;             % Total length of domain (CPU + heat sink), m
 L_CPU = 3/100;         % Length of CPU region, m
 thick = 0.3/100;       % Thickness of heat sink fin, m
@@ -17,7 +17,7 @@ specific_heat_Metal = 895.0;  % Specific heat of metal, J/(kg*K)
 alpha_Metal = k_Metal/(density_Metal*specific_heat_Metal);   % Thermal diffusivity of Metal, m^2/s
 %====NOTE: k_CPU is the uncertain input variable
 %===========================================================
-k_CPU =0.20;                  % Thermal conductivity of CPU, W/(m*K)
+k_CPU =0.40;                  % Thermal conductivity of CPU, W/(m*K)
 %===========================================================
 density_CPU = 2000.;          % Density of CPU, kg/m^3
 specific_heat_CPU = 750.;     % Specific heat of CPU, J/(kg*K)
